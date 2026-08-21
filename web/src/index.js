b7a8ae3e9863a671f22c8a1f2d5c691439df503a
Leandro <leandro.perez@andromeda360.ai>
2026-08-21 10:52:26 -0400

    fix(web): app route

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,7 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
-import App from './app'
+import App from './App'
 import ErrorBoundary from './components/ErrorBoundary'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
